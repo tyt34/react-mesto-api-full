f8b2472fe96e7edfb3e1272ff8832c4285fae718
R N <novoto4in@mail.ru>
2021-11-06 23:06:47 +0300

    0.04
    send right cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const options = {
   credentials: true,
 };
 
-app.use('*', cors(options));
+app.use('*', cors(options.origin));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
